cba20d838d8905baa22150271ea5060c53c64b4b
Ahmet Durak <98670034+AhmetDurak@users.noreply.github.com>
2022-07-23 23:56:19 +0200

    Update Hooks.java
    @After tag is imported through junit. That's the reason why closeDriver doesn't work
    It is changed to the io.cucumber.java.After

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,9 +1,9 @@
 package com.cydeo.step_definitions;
 
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
-import org.junit.After;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
